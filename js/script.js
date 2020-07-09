ca4ab51f9bf0df1c9e93223ebfe08d0957ddef64
mohammad <sabz818m@gmail.com>
2020-07-09 19:50:38 +0430

    add footer

--- js/script.js
+++ js/script.js
@@ -5,13 +5,3 @@ setTimeout(function () {
     spinner.style.display = 'none';
     main.style.display = 'block';
 }, 1500)
-// var myVar;
-
-// function myFunction() {
-//   myVar = setTimeout(showPage, 3000);
-// }
-
-// function showPage() {
-//   document.getElementById("minr").style.display = "none";
-//   document.getElementById("myDiv").style.display = "block";
-// }
